eb1dd7f88705af83230006c4314f3ed54c9d6a33
Jayneel Kanungo <jayneelkanungo57@gmail.com>
2024-06-15 00:52:10 +0530

    - clean code

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:neru_lending_app/presentation/splash/screen/splash_screen.dart';
-import 'package:neru_lending_app/utils/constant_colors.dart';
 import 'package:neru_lending_app/utils/constant_fonts.dart';
 
 void main() {
